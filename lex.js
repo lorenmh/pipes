473c3f0589c18fa84374f2de6359b13626ce1476
loren <neutronstar@gmail.com>
2016-05-19 15:15:52 -0700

    removed tabs

--- lex.js
+++ lex.js
@@ -4,46 +4,46 @@
  * @param {String} value - the string value of the token
  */
 function Token(type, value) {
-	return {
-		type: type,
-		value: value
-	};
+  return {
+    type: type,
+    value: value
+  };
 }
 
 // an enumeration of type constants
 var eni = 0,
-		TYPE = {
-			value:							eni++,
-			stringExpansion:		eni++,
-			stringRaw:					eni++,
-			pipe:								eni++,
-			directExpansion:		eni++,
-			indirectExpansion:	eni++,
+    TYPE = {
+      value:              eni++,
+      stringExpansion:    eni++,
+      stringRaw:          eni++,
+      pipe:               eni++,
+      directExpansion:    eni++,
+      indirectExpansion:  eni++,
 
-			invalid: {
-				value:							eni++,
-				stringExpansion:		eni++,
-				stringRaw:					eni++,
-				indirectExpansion:	eni++
-			}
-		}
+      invalid: {
+        value:              eni++,
+        stringExpansion:    eni++,
+        stringRaw:          eni++,
+        indirectExpansion:  eni++
+      }
+    }
 ;
 
 var TYPE_INT_MAP = {
-	VALUE:											TYPE.value,
-	STRING_EXPANSION:						TYPE.stringExpansion,
-	STRING_RAW:									TYPE.stringRaw,
-	PIPE:												TYPE.pipe,
-	INVALID_VALUE:							TYPE.invalid.value,
-	INVALID_STRING_EXPANSION:		TYPE.invalid.stringExpansion,
-	INVALID_STRING_RAW:					TYPE.invalid.stringRaw
+  VALUE:                      TYPE.value,
+  STRING_EXPANSION:           TYPE.stringExpansion,
+  STRING_RAW:                 TYPE.stringRaw,
+  PIPE:                       TYPE.pipe,
+  INVALID_VALUE:              TYPE.invalid.value,
+  INVALID_STRING_EXPANSION:   TYPE.invalid.stringExpansion,
+  INVALID_STRING_RAW:         TYPE.invalid.stringRaw
 };
 
 var INT_TYPE_MAP = Object.keys(TYPE_INT_MAP)
-		.reduce((map, k) => {
-			map[TYPE_INT_MAP[k]] = k;
-			return map;
-		}, {})
+    .reduce((map, k) => {
+      map[TYPE_INT_MAP[k]] = k;
+      return map;
+    }, {})
 ;
 
 var ttoi = (t) => TYPE_INT_MAP[t];
@@ -51,17 +51,17 @@ var itot = (i) => INT_TYPE_MAP[i];
 
 // regex to match individual characters
 var RE_EXPANSION = /\$/,
-		RE_IDENTIFIER = /\w/,
-		RE_CURLY_OPEN = /\{/,
-		RE_CURLY_CLOSE = /\}/,
-		RE_PAREN_OPEN = /\(/,
-		RE_PAREN_CLOSE = /\)/,
-		RE_VALUE = /[^\s\$\|'"]/,
-		RE_SPACE = /\s/,
-		RE_ESCAPE = /\\/,
-		RE_STRING_EXPANSION = /"/,
-		RE_STRING_RAW = /'/,
-		RE_PIPE = /\|/
+    RE_IDENTIFIER = /\w/,
+    RE_CURLY_OPEN = /\{/,
+    RE_CURLY_CLOSE = /\}/,
+    RE_PAREN_OPEN = /\(/,
+    RE_PAREN_CLOSE = /\)/,
+    RE_VALUE = /[^\s\$\|'"]/,
+    RE_SPACE = /\s/,
+    RE_ESCAPE = /\\/,
+    RE_STRING_EXPANSION = /"/,
+    RE_STRING_RAW = /'/,
+    RE_PIPE = /\|/
 ;
 
 /**
@@ -70,154 +70,154 @@ var RE_EXPANSION = /\$/,
  * @param {String} input - the string to be parsed
  */
 function lex(input) {
-	var cursor = 0, // start index
-			len = input.length,
-			tokens = [],
-			token, subcursor
-	;
+  var cursor = 0, // start index
+      len = input.length,
+      tokens = [],
+      token, subcursor
+  ;
 
-	// finite state machine
-	var debug=0;
-	main:
-	while (true) {
-		if (cursor >= len) { break main; }
+  // finite state machine
+  var debug=0;
+  main:
+  while (true) {
+    if (cursor >= len) { break main; }
 
-		if (input[cursor].match(RE_SPACE)) {
-			cursor++;
-			continue main;
-		}
+    if (input[cursor].match(RE_SPACE)) {
+      cursor++;
+      continue main;
+    }
 
-		// Value
-		if (input[cursor].match(RE_VALUE)) {
-			subcursor = cursor + 1;
+    // Value
+    if (input[cursor].match(RE_VALUE)) {
+      subcursor = cursor + 1;
 
-			value:
-			while (true) {
-				if (subcursor >= len) {
-					token = Token(
-						TYPE.value,
-						input.substring(cursor, subcursor)
-					);
+      value:
+      while (true) {
+        if (subcursor >= len) {
+          token = Token(
+            TYPE.value,
+            input.substring(cursor, subcursor)
+          );
 
-					tokens.push(token);
-					break main;
-				}
+          tokens.push(token);
+          break main;
+        }
 
-				if (!input[subcursor].match(RE_VALUE)) {
-					if (input[subcursor].match(RE_SPACE)) {
-						token = Token(
-							TYPE.value,
-							input.substring(cursor, subcursor)
-						);
-					} else {
-						token = Token(
-							TYPE.invalid.value,
-							input.substring(cursor, subcursor)
-						);
-					}
-					tokens.push(token);
-					cursor = subcursor + 1;
-					continue main;
-				}
+        if (!input[subcursor].match(RE_VALUE)) {
+          if (input[subcursor].match(RE_SPACE)) {
+            token = Token(
+              TYPE.value,
+              input.substring(cursor, subcursor)
+            );
+          } else {
+            token = Token(
+              TYPE.invalid.value,
+              input.substring(cursor, subcursor)
+            );
+          }
+          tokens.push(token);
+          cursor = subcursor + 1;
+          continue main;
+        }
 
-				subcursor++;
-			}
+        subcursor++;
+      }
 
-		}
+    }
 
-		// String expansion
-		if (input[cursor].match(RE_STRING_EXPANSION)) {
-			subcursor = cursor + 1;
+    // String expansion
+    if (input[cursor].match(RE_STRING_EXPANSION)) {
+      subcursor = cursor + 1;
 
-			stringExpansion:
-			while (true) {
-				if (subcursor >= len) {
-					token = Token(
-						TYPE.invalid.stringExpansion,
-						input.substring(cursor, subcursor)
-					);
+      stringExpansion:
+      while (true) {
+        if (subcursor >= len) {
+          token = Token(
+            TYPE.invalid.stringExpansion,
+            input.substring(cursor, subcursor)
+          );
 
-					tokens.push(token);
-					break main;
-				}
+          tokens.push(token);
+          break main;
+        }
 
-				if (input[subcursor].match(RE_ESCAPE)) {
-					subcursor += 2;
-					continue stringExpansion;
-				}
+        if (input[subcursor].match(RE_ESCAPE)) {
+          subcursor += 2;
+          continue stringExpansion;
+        }
 
-				if (input[subcursor].match(RE_STRING_EXPANSION)) {
-					subcursor++;
-					token = Token(
-						TYPE.stringExpansion,
-						input.substring(cursor, subcursor)
-					);
+        if (input[subcursor].match(RE_STRING_EXPANSION)) {
+          subcursor++;
+          token = Token(
+            TYPE.stringExpansion,
+            input.substring(cursor, subcursor)
+          );
 
-					tokens.push(token);
+          tokens.push(token);
 
-					cursor = subcursor;
-					continue main;
-				}
+          cursor = subcursor;
+          continue main;
+        }
 
-				subcursor++;
-			}
+        subcursor++;
+      }
 
-		}
+    }
 
-		// String raw
-		if (input[cursor].match(RE_STRING_RAW)) {
-			subcursor = cursor + 1;
+    // String raw
+    if (input[cursor].match(RE_STRING_RAW)) {
+      subcursor = cursor + 1;
 
-			stringRaw:
-			while (true) {
-				if (subcursor >= len) {
-					token = Token(
-						TYPE.invalid.stringRaw,
-						input.substring(cursor, subcursor)
-					);
+      stringRaw:
+      while (true) {
+        if (subcursor >= len) {
+          token = Token(
+            TYPE.invalid.stringRaw,
+            input.substring(cursor, subcursor)
+          );
 
-					tokens.push(token);
-					break main;
-				}
+          tokens.push(token);
+          break main;
+        }
 
-				if (input[subcursor].match(RE_ESCAPE)) {
-					subcursor += 2;
-					continue stringRaw;
-				}
+        if (input[subcursor].match(RE_ESCAPE)) {
+          subcursor += 2;
+          continue stringRaw;
+        }
 
-				if (input[subcursor].match(RE_STRING_RAW)) {
-					subcursor++;
-					token = Token(
-						TYPE.stringRaw,
-						input.substring(cursor, subcursor)
-					);
+        if (input[subcursor].match(RE_STRING_RAW)) {
+          subcursor++;
+          token = Token(
+            TYPE.stringRaw,
+            input.substring(cursor, subcursor)
+          );
 
-					tokens.push(token);
+          tokens.push(token);
 
-					cursor = subcursor;
-					continue main;
-				}
+          cursor = subcursor;
+          continue main;
+        }
 
-				subcursor++;
-			}
+        subcursor++;
+      }
 
-		}
+    }
 
-		// Pipe
-		if (input[cursor].match(RE_PIPE)) {
-			token = Token(
-				TYPE.pipe,
-				input.substring(cursor, cursor + 1)
-			);
+    // Pipe
+    if (input[cursor].match(RE_PIPE)) {
+      token = Token(
+        TYPE.pipe,
+        input.substring(cursor, cursor + 1)
+      );
 
-			tokens.push(token);
+      tokens.push(token);
 
-			cursor++;
+      cursor++;
 
-			continue main;
-		}
-	}
-	return tokens;
+      continue main;
+    }
+  }
+  return tokens;
 }
 
 var str = "re 'COEN\d{3}' | fmt 'Santa Clara University {{0}}' | google-url | nt";
