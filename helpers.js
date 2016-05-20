7b64c5e1a2aff718c4b9402c3dd918de76274be1
loren <neutronstar@gmail.com>
2016-05-19 17:35:37 -0700

    hrmmmm

---
+++ helpers.js
@@ -0,0 +1,9 @@
+function isFunc(v) {
+  return typeof(v) === 'function';
+}
+
+function isValue(v) {
+  return !(v === undefined || v === null);
+}
+
+module.exports = { isFunc };
--- pipe.js
+++ pipe.js
@@ -1,12 +1,56 @@
-// A command looks like:
-tre = function() {
-  function start() {
+const EOF = require('./const').EOF
+;
 
+var helpers = require('./helpers'),
+    isFunc = helpers.isFunc,
+    isValue = helpers.isValue
+;
+
+// breadth
+var l = [];
+function leaves(doc) {
+  var ch = []
+}
+
+function searchDocument(doc, output, error) {
+  var visited = {},
+      foundInChild = {}
+  ;
+
+  for (var i
+}
+
+// A command looks like:
+var tre = function(io, args) {
+  // var io = {
+  //   input: null,
+  //   output: output,
+  //   error: error,
+  //   commandError: commandError
+  //   start: start,
+  //   doc: null
+  // }
+
+  var re = new RegExp(args[0]);
+
+  io.execute = function execute() {
+    function fn(v) {
+      v = String(v);
+
+      if (v.match(re)) {
+        io.write(v);
+      }
+    }
+
+    if (isFunc(io.input)) {
+      input = io.input();
+      
+      if isValue(input) {
+        fn(v);
+      }
+    } else {
+      io.document
+    }
   }
 
-  return {
-    reader: null,
-    output: output,
-    error: error,
-    start: start,
 }
