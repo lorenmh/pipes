885a8662c7775aca44c02d1622ac13a9dcf3f19e
loren <neutronstar@gmail.com>
2016-05-20 11:37:59 -0700

    correctly searches a document and gets the matching nodes

--- pipe.js
+++ pipe.js
@@ -7,34 +7,32 @@ var helpers = require('./helpers'),
 ;
 
 // breadth
-var l = [];
-function getLeaves(doc) {
-  var leaves = [],
-      current = [doc.body],
-      next = []
-  ;
+// function getLeaves(doc) {
+//   var leaves = [],
+//       current = [doc.body],
+//       next = []
+//   ;
+// 
+//   while (current.length !== 0) {
+//     for (var i = 0; i < current.length; i++) {
+//       var node = current[i];
+//       var children = Array.prototype.slice.apply(node.children);
+// 
+//       if (children.length) {
+//         next = next.concat(children);
+//       } else {
+//         leaves.push(node);
+//       }
+//     }
+//     current = next;
+//     next = [];
+//   }
+// 
+//   return leaves;
+// }
 
-  while (current.length !== 0) {
-    for (var i = 0; i < current.length; i++) {
-      var node = current[i];
-      var children = Array.prototype.slice.apply(node.children);
-
-      if (children.length) {
-        next = next.concat(children);
-      } else {
-        leaves.push(node);
-      }
-    }
-    current = next;
-    next = [];
-  }
-
-  return leaves;
-}
-
-function idGenerator() {
+var id = (function() {
   var index = 0;
-
   return function idForEl(el) {
     if (el.$$searchId$$ === undefined) {
       return (el.$$searchId$$ = index++);
@@ -42,67 +40,147 @@ function idGenerator() {
       return el.$$searchId$$;
     }
   };
+})();
+
+function Depth() {
+  this.exists = {};
+  this.nodes = [];
 }
 
-var visited, foundInChild = {}, found = [], current, next, inNext
-function searchDocument(doc, re, output, error) {
-  visited = {}
-  foundInChild = {}
-  found = []
-  
+Depth.factory = function DepthFactory() {
+  return new Depth();
+};
 
-  current = getLeaves(doc);
-  next = [];
-  inNext = {};
-  var node, nodeId, match, parent, parentId;
+Depth.prototype.insert = function insert(n) {
+  if (!n) { return; }
 
-  var idForEl = idGenerator();
+  var nodeId = id(n);
 
+  if (!this.exists[nodeId]) {
+    this.exists[nodeId] = true;
+    this.nodes.push(n);
+  }
+};
+
+function getDepths(doc) {
+  var depths = [],
+      current = [doc],
+      next = [],
+      node, children, nodeId
+  ;
+
+  var depth = 0;
   while (current.length !== 0) {
+    depths[depth] = Depth.factory();
+
     for (var i = 0; i < current.length; i++) {
       node = current[i];
-      nodeId = idForEl(node);
+      children = Array.prototype.slice.apply(node.children);
+
+      if (children.length) {
+        next = next.concat(children);
+      } else {
+        depths[depth].insert(node);
+      }
+    }
+    current = next;
+    next = [];
+    depth++;
+  }
+
+  return depths;
+}
+
+function searchDocument(doc, re, output, error) {
+  var visited = {},
+      foundInChild = {},
+      found = [],
+      depths = getDepths(document),
+      depth, node, nodeId, match, parent, parentId, parentDepth
+  ;
+  for (var i = depths.length - 1; i >= 0; i--) {
+    depth = depths[i];
+    for (var j = 0; j < depth.nodes.length; j++) {
+      node = depth.nodes[j];
+      nodeId = id(node);
 
       if (!visited[nodeId]) {
         visited[nodeId] = true;
         if (!foundInChild[nodeId]) {
           match = node.textContent.match(re);
           if (match) {
+            console.log('matched');
             parent = node;
-            do {
+            while (parent.parentElement) {
               parent = parent.parentElement;
-              if (parent !== null) {
-                parentId = idForEl(parent);
-                if (foundInChild[parentId]) {
-                  break;
-                }
-                foundInChild[parentId] = true;
+              parentId = id(parent);
+              if (foundInChild[parentId]) {
+                break;
               }
-            } while(parent !== doc && parent !== null);
-
-            //while(node.nodeName === '#text' && node.parentElement !== null) {
-              //node = node.parentElement;
-            //}
-
+              foundInChild[parentId] = true;
+            }
             found.push(node);
           } else {
-            parent = node.parentElement;
-            parentId = idForEl(parent);
-            if (parent && !inNext[parentId]) {
-              next.push(parent);
-              inNext[parentId] = true;
+            parentDepth = i - 1;
+            if (parentDepth >= 0) {
+              parent = node.parentElement;
+              depths[parentDepth].insert(parent);
             }
           }
         }
       }
     }
-
-    current = next;
-    next = [];
   }
 
   return found;
 }
+// 
+// 
+//   while (current.length !== 0) {
+//     for (var i = 0; i < current.length; i++) {
+//       node = current[i];
+//       nodeId = idForEl(node);
+// 
+//       if (!visited[nodeId]) {
+//         visited[nodeId] = true;
+//         if (!foundInChild[nodeId]) {
+//           match = node.textContent.match(re);
+//           if (match) {
+//             parent = node;
+//             do {
+//               parent = parent.parentElement;
+//               if (parent !== null) {
+//                 parentId = idForEl(parent);
+//                 if (foundInChild[parentId]) {
+//                   break;
+//                 }
+//                 foundInChild[parentId] = true;
+//               }
+//             } while(parent !== doc && parent !== null);
+// 
+//             //while(node.nodeName === '#text' && node.parentElement !== null) {
+//               //node = node.parentElement;
+//             //}
+// 
+//             found.push(node);
+//           } else {
+//             parent = node.parentElement;
+//             parentId = idForEl(parent);
+//             if (parent && !inNext[parentId]) {
+//               next.push(parent);
+//               inNext[parentId] = true;
+//             }
+//           }
+//         }
+//       }
+//     }
+// 
+//     current = next;
+//     next = [];
+//   }
+// 
+//   return found;
+// }
 
 // A command looks like:
 var tre = function(io, args) {
